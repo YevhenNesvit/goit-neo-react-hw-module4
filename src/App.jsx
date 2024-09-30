61eec30fc1991cef5f23e6dce1e778f3b14f267b
Yevhen Nesvit <yevhen.nesvit@gmail.com>
2024-09-30 15:12:01 +0300

    app correction

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
-import { useState, useEffect } from 'react';
-import axios from 'axios';
-import SearchBar from './components/SearchBar/SearchBar';
-import ImageGallery from './components/ImageGallery/ImageGallery';
-import Loader from './components/Loader/Loader';
-import LoadMoreBtn from './components/LoadMoreBtn/LoadMoreBtn';
-import ImageModal from './components/ImageModal/ImageModal';
-import * as styles from './App.css';
+import { useState, useEffect } from "react";
+import axios from "axios";
+import SearchBar from "./components/SearchBar/SearchBar";
+import ImageGallery from "./components/ImageGallery/ImageGallery";
+import Loader from "./components/Loader/Loader";
+import LoadMoreBtn from "./components/LoadMoreBtn/LoadMoreBtn";
+import ImageModal from "./components/ImageModal/ImageModal";
+import ErrorMessage from "./components/ErrorMessage/ErrorMessage";
+import * as styles from "./App.css";
 
 const App = () => {
   const [images, setImages] = useState([]);
-  const [query, setQuery] = useState('');
+  const [query, setQuery] = useState("");
   const [page, setPage] = useState(1);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
@@ -20,41 +21,36 @@ const App = () => {
 
     const fetchImages = async () => {
       setLoading(true);
-      try {
-        const response = await axios.get(
-          `https://api.unsplash.com/search/photos`,
-          {
-            params: {
-              query,
-              page,
-              per_page: 12,
-              client_id: 'IOSomVG1XQbYOHUXK4cpam1ynLzZW16kpz6BjJvANMQ', // Replace with your access key
-            },
-          }
-        );
-        setImages(prevImages => [...prevImages, ...response.data.results]);
-        setError(null);
-      } catch {
-        setError('Failed to fetch images. Please try again.');
-      } finally {
-        setLoading(false);
-      }
+      const response = await axios.get(
+        `https://api.unsplash.com/search/photos`,
+        {
+          params: {
+            query,
+            page,
+            per_page: 12,
+            client_id: "IOSomVG1XQbYOHUXK4cpam1ynLzZW16kpz6BjJvANMQ", // Replace with your access key
+          },
+        }
+      );
+      setImages((prevImages) => [...prevImages, ...response.data.results]);
+      setError(null);
+      setLoading(false);
     };
 
     fetchImages();
   }, [query, page]);
 
-  const handleSearchSubmit = searchQuery => {
+  const handleSearchSubmit = (searchQuery) => {
     setQuery(searchQuery);
     setImages([]);
     setPage(1);
   };
 
   const handleLoadMore = () => {
-    setPage(prevPage => prevPage + 1);
+    setPage((prevPage) => prevPage + 1);
   };
 
-  const handleImageClick = image => {
+  const handleImageClick = (image) => {
     setSelectedImage(image); // Set the clicked image as selected
   };
 
@@ -65,11 +61,15 @@ const App = () => {
   return (
     <div className={styles.app}>
       <SearchBar onSubmit={handleSearchSubmit} />
-      {error && <p>{error}</p>}
+      {error && <ErrorMessage />}
       <ImageGallery images={images} onImageClick={handleImageClick} />
       {loading && <Loader />}
-      {images.length > 0 && !loading && <LoadMoreBtn onClick={handleLoadMore} />}
-      {selectedImage && <ImageModal image={selectedImage} onClose={closeModal} />}
+      {images.length > 0 && !loading && (
+        <LoadMoreBtn onClick={handleLoadMore} />
+      )}
+      {selectedImage && (
+        <ImageModal image={selectedImage} onClose={closeModal} />
+      )}
     </div>
   );
 };
